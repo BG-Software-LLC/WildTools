7399c6de112957e7f8bb5b11f3258cad310d4522
OmerBenGera <omerthepro126@gmail.com>
2019-04-20 19:56:34 +0300

    Added all material types for shovels and axes

--- src/main/java/com/bgsoftware/wildtools/listeners/BlocksListener.java
+++ src/main/java/com/bgsoftware/wildtools/listeners/BlocksListener.java
@@ -3,6 +3,8 @@ package com.bgsoftware.wildtools.listeners;
 import com.bgsoftware.wildtools.Locale;
 import com.bgsoftware.wildtools.WildToolsPlugin;
 import com.bgsoftware.wildtools.objects.tools.WTool;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.EventHandler;
@@ -23,8 +25,26 @@ import java.util.UUID;
 public final class BlocksListener implements Listener {
 
     private static Map<UUID, Material> lastClickedType = new HashMap<>();
-    private static String[] shovelMaterials = new String[] {"DIRT"};
-    private static String[] axeMaterials = new String[] {"LOG"};
+    private static String[] shovelMaterials = new String[] {"CLAY", "SOIL", "GRASS", "GRASS_PATH", "GRAVEL", "MYCEL",
+            "SOUL_SAND", "SAND", "DIRT", "SNOW_BLOCK", "SNOW", "FARMLAND", "COARSE_DIRT", "GRASS_BLOCK",
+            "MYCELIUM", "PODZOL", "RED_SAND", ""};
+    private static String[] axeMaterials = new String[] {"TRAP_DOOR", "WOODEN_DOOR", "SPRUCE_DOOR", "BIRCH_DOOR",
+            "JUNGLE_DOOR", "ACACIA_DOOR", "DARK_OAK_DOOR", "TRAPPED_CHEST", "FENCE", "SPRUCE_FENCE", "BIRCH_FENCE",
+            "JUNGLE_FENCE", "ACACIA_FENCE", "DARK_OAK_FENCE", "CHEST", "FENCE_GATE", "SPRUCE_FENCE_GATE", "BIRCH_FENCE_GATE",
+            "JUNGLE_FENCE_GATE", "ACACIA_FENCE_GATE", "DARK_OAK_FENCE_GATE", "WORKBENCH", "LOG", "LOG_2", "JUKEBOX",
+            "WOOD", "WOOD_STEP", "WOOD_STAIRS", "SPRUCE_WOOD_STAIRS", "BIRCH_WOOD_STAIRS", "JUNGLE_WOOD_STAIRS",
+            "ACACIA_STAIRS", "DARK_OAK_STAIRS", "BOOKSHELF", "STANDING_BANNER", "PUMPKIN", "JACK_O_LANTERN", "NOTE_BLOCK",
+            "SIGN_POST", "WALL_SIGN", "DAYLIGHT_DETECTOR", "HUGE_MUSHROOM_1", "HUGE_MUSHROOM_2", "OAK_DOOR", "OAK_TRAPDOOR",
+            "SPRUCE_TRAPDOOR", "BIRCH_TRAPDOOR", "JUNGLE_TRAPDOOR", "ACACIA_TRAPDOOR", "DARK_OAK_TRAPDOOR", "OAK_FENCE",
+            "OAK_FENCE_GATE", "OAK_LOG", "SPRUCE_LOG", "BIRCH_LOG", "JUNGLE_LOG", "ACACIA_LOG", "DARK_OAK_LOG", "STRIPPED_OAK_LOG",
+            "STRIPPED_SPRUCE_LOG", "STRIPPED_BIRCH_LOG", "STRIPPED_JUNGLE_LOG", "STRIPPED_ACACIA_LOG", "STRIPPED_DARK_OAK_LOG",
+            "OAK_PLANKS", "SPRUCE_PLANKS", "BIRCH_PLANKS", "JUNGLE_PLANKS", "ACACIA_PLANKS", "DARK_OAK_PLANKS", "OAK_SLAB",
+            "SPRUCE_SLAB", "BIRCH_SLAB", "JUNGLE_SLAB", "ACACIA_SLAB", "DARK_OAK_SLAB", "OAK_STAIRS", "BIRCH_STAIRS",
+            "JUNGLE_STAIRS", "OAK_PRESSURE_PLATE", "SPRUCE_PRESSURE_PLATE", "BIRCH_PRESSURE_PLATE", "JUNGLE_PRESSURE_PLATE",
+            "ACACIA_PRESSURE_PLATE", "DARK_OAK_PRESSURE_PLATE", "CRAFTING_TABLE", "CARVED_PUMPKIN", "WHITE_BANNER",
+            "ORANGE_BANNER", "MAGENTA_BANNER", "LIGHT_BLUE_BANNER", "YELLOW_BANNER", "LIME_BANNER", "PINK_BANNER",
+            "GRAY_BANNER", "LIGHT_GRAY_BANNER", "CYAN_BANNER", "PURPLE_BANNER", "BLUE_BANNER", "BROWN_BANNER", "SPRUCE_STAIRS",
+            "GREEN_BANNER", "RED_BANNER", "BLACK_BANNER", "SIGN", "MUSHROOM_STEM", "RED_MUSHROOM_BLOCK", "BROWN_MUSHROOM_BLOCK"};
 
     private WildToolsPlugin instance;
 
@@ -117,13 +137,11 @@ public final class BlocksListener implements Listener {
         if(tool == null || !tool.isOmni())
             return;
 
-        e.setCancelled(true);
-
         lastClickedType.put(e.getPlayer().getUniqueId(), e.getClickedBlock().getType());
 
         String replaceType = "PICKAXE";
         if(Arrays.asList(shovelMaterials).contains(e.getClickedBlock().getType().name())){
-            replaceType = "SPADE";
+            replaceType = instance.getNMSAdapter().getVersion().contains("1_13") ? "SHOVEL" : "SPADE";
         }
         else if(Arrays.asList(axeMaterials).contains(e.getClickedBlock().getType().name())){
             replaceType = "AXE";
