943ee5c6828819bbebe0abb7b5dcd69b0c01b1d4
OmerBenGera <omerthepro126@gmail.com>
2019-12-20 17:49:26 +0200

    Fixed prices of ShopGUIPlus

--- src/main/java/com/bgsoftware/wildtools/hooks/PricesProvider_ShopGUIPlus.java
+++ src/main/java/com/bgsoftware/wildtools/hooks/PricesProvider_ShopGUIPlus.java
@@ -2,15 +2,10 @@ package com.bgsoftware.wildtools.hooks;
 
 import com.bgsoftware.wildtools.WildToolsPlugin;
 import net.brcdev.shopgui.ShopGuiPlugin;
-import net.brcdev.shopgui.player.PlayerData;
-import net.brcdev.shopgui.player.PlayerManager;
-import net.brcdev.shopgui.shop.Shop;
-import net.brcdev.shopgui.shop.ShopItem;
+import net.brcdev.shopgui.ShopGuiPlusApi;
 
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.Map;
 
 public final class PricesProvider_ShopGUIPlus implements PricesProvider {
 
@@ -18,66 +13,16 @@ public final class PricesProvider_ShopGUIPlus implements PricesProvider {
 
     public PricesProvider_ShopGUIPlus(){
         WildToolsPlugin.log(" - Using ShopGUIPlus as PricesProvider.");
-        //Loading database of online players
         plugin = ShopGuiPlugin.getInstance();
-        for(Player player : WildToolsPlugin.getPlugin().getNMSAdapter().getOnlinePlayers()) {
-            try {
-                //noinspection JavaReflectionMemberAccess
-                PlayerManager.class.getMethod("loadData", Player.class).invoke(plugin.getPlayerManager(), player);
-            }catch(Throwable ex){
-                plugin.getPlayerManager().loadData(player, new net.brcdev.shopgui.database.Callback() {
-                    @Override
-                    public void onSuccess(Object o) {
-
-                    }
-
-                    @Override
-                    public void onFailure(Object o) {
-
-                    }
-                });
-            }
-        }
     }
 
     @Override
     public double getPrice(Player player, ItemStack itemStack) {
         try {
-            PlayerData playerData;
-
-            try{
-                playerData = plugin.getPlayerManager().getPlayerData(player);
-            }catch(NullPointerException ex){
-                plugin.getPlayerManager().registerPlayer(player);
-                playerData = plugin.getPlayerManager().getPlayerData(player);
-            }
-
-            double price = 0;
-
-            Map<String, Shop> shops = plugin.getShopManager().shops;
-            for(Shop shop : shops.values()){
-                for(ShopItem shopItem : shop.getShopItems()){
-                    if(shopItem.getItem().isSimilar(itemStack) && getSellPrice(shopItem, shop, playerData, player, itemStack.getAmount()) > price)
-                        price = getSellPrice(shopItem, shop, playerData, player, itemStack.getAmount());
-                }
-            }
-            return price;
+            return ShopGuiPlusApi.getItemStackPriceSell(player, itemStack);
         }catch(Exception ex){
-            return -1;
-        }
-    }
-
-    private double getSellPrice(ShopItem shopItem, Shop shop, PlayerData playerData, Player player, int amount){
-        try {
-            return shopItem.getSellPriceForAmount(shop, player, playerData, amount);
-        }catch(NoSuchMethodError ex){
-            try {
-                //noinspection JavaReflectionMemberAccess
-                return (double) ShopItem.class.getMethod("getSellPriceForAmount", Shop.class, PlayerData.class, int.class).invoke(shopItem, shop, playerData, amount);
-            }catch(Exception ex1){
-                ex1.printStackTrace();
-                return 0;
-            }
+            plugin.getPlayerManager().registerPlayer(player);
+            return ShopGuiPlusApi.getItemStackPriceSell(player, itemStack);
         }
     }
 
