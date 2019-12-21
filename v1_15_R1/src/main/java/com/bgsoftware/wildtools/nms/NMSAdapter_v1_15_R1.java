a87b1c6021c61b0983f804c026ae36770ac27173
OmerBenGera <omerthepro126@gmail.com>
2019-12-21 18:52:47 +0200

    Added support for 1.15

---
+++ v1_15_R1/src/main/java/com/bgsoftware/wildtools/nms/NMSAdapter_v1_15_R1.java
@@ -0,0 +1,347 @@
+package com.bgsoftware.wildtools.nms;
+
+import com.bgsoftware.wildtools.objects.WMaterial;
+import net.minecraft.server.v1_15_R1.Block;
+import net.minecraft.server.v1_15_R1.BlockPosition;
+import net.minecraft.server.v1_15_R1.Chunk;
+import net.minecraft.server.v1_15_R1.ChunkProviderServer;
+import net.minecraft.server.v1_15_R1.EntityHuman;
+import net.minecraft.server.v1_15_R1.EntityItem;
+import net.minecraft.server.v1_15_R1.EntityLiving;
+import net.minecraft.server.v1_15_R1.EntityPlayer;
+import net.minecraft.server.v1_15_R1.IBlockData;
+import net.minecraft.server.v1_15_R1.ItemStack;
+import net.minecraft.server.v1_15_R1.LightEngineThreaded;
+import net.minecraft.server.v1_15_R1.NBTTagCompound;
+import net.minecraft.server.v1_15_R1.PacketPlayOutCollect;
+import net.minecraft.server.v1_15_R1.PacketPlayOutMapChunk;
+import net.minecraft.server.v1_15_R1.PlayerInventory;
+import net.minecraft.server.v1_15_R1.TileEntity;
+import net.minecraft.server.v1_15_R1.World;
+import net.minecraft.server.v1_15_R1.WorldServer;
+import org.bukkit.Bukkit;
+import org.bukkit.CropState;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.WorldBorder;
+import org.bukkit.block.data.Ageable;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.craftbukkit.v1_15_R1.CraftChunk;
+import org.bukkit.craftbukkit.v1_15_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_15_R1.block.CraftBlock;
+import org.bukkit.craftbukkit.v1_15_R1.block.data.CraftBlockData;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftItem;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftLivingEntity;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftInventoryPlayer;
+import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftItemStack;
+import org.bukkit.craftbukkit.v1_15_R1.util.CraftMagicNumbers;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SuppressWarnings({"unused", "ConstantConditions"})
+public final class NMSAdapter_v1_15_R1 implements NMSAdapter {
+
+    @Override
+    public String getVersion() {
+        return "v1_15_R1";
+    }
+
+    @Override
+    public boolean isLegacy() {
+        return false;
+    }
+
+    @Override
+    public List<org.bukkit.inventory.ItemStack> getBlockDrops(Player pl, org.bukkit.block.Block bl, boolean silkTouch) {
+        List<org.bukkit.inventory.ItemStack> drops = new ArrayList<>();
+
+        EntityPlayer player = ((CraftPlayer) pl).getHandle();
+        BlockPosition blockPosition = new BlockPosition(bl.getX(), bl.getY(), bl.getZ());
+        WorldServer worldServer = player.playerInteractManager.world;
+        IBlockData blockData = worldServer.getType(blockPosition);
+        Block block = blockData.getBlock();
+        ItemStack itemStack = player.getItemInMainHand();
+        itemStack = itemStack.isEmpty() ? ItemStack.a : itemStack.cloneItemStack();
+        TileEntity tileEntity = worldServer.getTileEntity(blockPosition);
+
+        return Block.getDrops(blockData, worldServer, blockPosition, tileEntity, player, itemStack.isEmpty() ? ItemStack.a : itemStack.cloneItemStack())
+                .stream().map(CraftItemStack::asBukkitCopy).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<org.bukkit.inventory.ItemStack> getCropDrops(Player pl, org.bukkit.block.Block bl) {
+        //1.14 has a nice method to get drops of blocks, so we can safely call the getBlockDrops method.
+        return getBlockDrops(pl, bl, false);
+    }
+
+    @Override
+    public int getTag(org.bukkit.inventory.ItemStack is, String key, int def) {
+        ItemStack nmsStack = CraftItemStack.asNMSCopy(is);
+
+        if(nmsStack == null)
+            return def;
+
+        NBTTagCompound tag = new NBTTagCompound();
+
+        if(nmsStack.hasTag()){
+            tag = nmsStack.getTag();
+        }
+
+        if(tag.hasKey(key)){
+            return tag.getInt(key);
+        }
+
+        return def;
+    }
+
+    @Override
+    public org.bukkit.inventory.ItemStack setTag(org.bukkit.inventory.ItemStack is, String key, int value) {
+        ItemStack nmsStack = CraftItemStack.asNMSCopy(is);
+        NBTTagCompound tag = new NBTTagCompound();
+
+        if(nmsStack.hasTag()){
+            tag = nmsStack.getTag();
+        }
+
+        tag.setInt(key, value);
+
+        nmsStack.setTag(tag);
+
+        return CraftItemStack.asBukkitCopy(nmsStack);
+    }
+
+    @Override
+    public String getTag(org.bukkit.inventory.ItemStack is, String key, String def) {
+        ItemStack nmsStack = CraftItemStack.asNMSCopy(is);
+
+        if(nmsStack == null)
+            return def;
+
+        NBTTagCompound tag = new NBTTagCompound();
+
+        if(nmsStack.hasTag()){
+            tag = nmsStack.getTag();
+        }
+
+        if(tag.hasKey(key)){
+            return tag.getString(key);
+        }
+
+        return def;
+    }
+
+    @Override
+    public org.bukkit.inventory.ItemStack setTag(org.bukkit.inventory.ItemStack is, String key, String value) {
+        ItemStack nmsStack = CraftItemStack.asNMSCopy(is);
+        NBTTagCompound tag = new NBTTagCompound();
+
+        if(nmsStack.hasTag()){
+            tag = nmsStack.getTag();
+        }
+
+        tag.setString(key, value);
+
+        nmsStack.setTag(tag);
+
+        return CraftItemStack.asBukkitCopy(nmsStack);
+    }
+
+    @Override
+    public org.bukkit.inventory.ItemStack getItemInHand(Player player) {
+        ItemStack itemStack = ((CraftInventoryPlayer) player.getInventory()).getInventory().getItemInHand();
+        return CraftItemStack.asBukkitCopy(itemStack);
+    }
+
+    @Override
+    public void setItemInHand(Player player, org.bukkit.inventory.ItemStack itemStack) {
+        PlayerInventory playerInventory = ((CraftInventoryPlayer) player.getInventory()).getInventory();
+        playerInventory.setItem(playerInventory.itemInHandIndex, CraftItemStack.asNMSCopy(itemStack));
+    }
+
+    @Override
+    public boolean isFullyGrown(org.bukkit.block.Block block) {
+        if(block.getType() == Material.CACTUS || block.getType() == WMaterial.SUGAR_CANE.parseMaterial() ||
+            block.getType() == Material.PUMPKIN || block.getType() == WMaterial.MELON.parseMaterial() ||
+                block.getType().name().equals("BAMBOO"))
+            return true;
+        CraftBlock craftBlock = (CraftBlock) block;
+        BlockData blockData = craftBlock.getBlockData();
+        return ((Ageable) blockData).getAge() == ((Ageable) blockData).getMaximumAge();
+    }
+
+    @Override
+    public void setCropState(org.bukkit.block.Block block, CropState cropState) {
+        if(block.getType() == Material.CHORUS_PLANT){
+            block.setType(Material.CHORUS_FLOWER);
+        }
+        else {
+            CraftBlock craftBlock = (CraftBlock) block;
+            BlockData blockData = craftBlock.getBlockData();
+            if(blockData instanceof Ageable) {
+                ((Ageable) blockData).setAge(cropState.ordinal());
+                craftBlock.setBlockData(blockData, true);
+            }
+            else{
+                block.setType(Material.AIR);
+            }
+        }
+    }
+
+    @Override
+    public void copyBlock(org.bukkit.block.Block from, org.bukkit.block.Block to) {
+        CraftBlock fromBlock = (CraftBlock) from, toBlock = (CraftBlock) to;
+        toBlock.setType(fromBlock.getType());
+        toBlock.setBlockData(fromBlock.getBlockData(), true);
+    }
+
+    @Override
+    public Collection<Player> getOnlinePlayers() {
+        return new ArrayList<>(Bukkit.getOnlinePlayers());
+    }
+
+    @Override
+    public void setBlockFast(Location location, int combinedId) {
+        World world = ((CraftWorld) location.getWorld()).getHandle();
+        Chunk chunk = world.getChunkAt(location.getChunk().getX(), location.getChunk().getZ());
+        BlockPosition blockPosition = new BlockPosition(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+        chunk.setType(blockPosition, Block.getByCombinedId(combinedId), true);
+    }
+
+    @Override
+    public void refreshChunks(List<org.bukkit.Chunk> chunksList) {
+        if(chunksList.size() == 0)
+            return;
+
+        World world = ((CraftWorld) chunksList.get(0).getWorld()).getHandle();
+        ChunkProviderServer chunkProvider = (ChunkProviderServer) world.getChunkProvider();
+        LightEngineThreaded lightEngineThreaded = chunkProvider.getLightEngine();
+        for(org.bukkit.Chunk bukkitChunk : chunksList){
+            Chunk chunk = ((CraftChunk) bukkitChunk).getHandle();
+            PacketPlayOutMapChunk packetPlayOutMapChunk = new PacketPlayOutMapChunk(chunk, 65535);
+            for(EntityHuman entityHuman : world.getPlayers())
+                ((EntityPlayer) entityHuman).playerConnection.sendPacket(packetPlayOutMapChunk);
+        }
+    }
+
+    @Override
+    public int getCombinedId(Location location) {
+        World world = ((CraftWorld) location.getWorld()).getHandle();
+        BlockPosition blockPosition = new BlockPosition(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+        return Block.getCombinedId(world.getType(blockPosition));
+    }
+
+    @Override
+    public void setCombinedId(Location location, int combinedId) {
+        World world = ((CraftWorld) location.getWorld()).getHandle();
+        BlockPosition blockPosition = new BlockPosition(location.getBlockX(), location.getBlockY(), location.getBlockZ());
+        world.setTypeAndData(blockPosition, Block.getByCombinedId(combinedId), 18);
+    }
+
+    @Override
+    public Enchantment getGlowEnchant() {
+        //noinspection NullableProblems
+        return new Enchantment(NamespacedKey.minecraft("glowing_enchant")) {
+            @Override
+            public String getName() {
+                return "WildToolsGlow";
+            }
+
+            @Override
+            public int getMaxLevel() {
+                return 1;
+            }
+
+            @Override
+            public int getStartLevel() {
+                return 0;
+            }
+
+            @Override
+            public EnchantmentTarget getItemTarget() {
+                return null;
+            }
+
+            @Override
+            public boolean conflictsWith(Enchantment enchantment) {
+                return false;
+            }
+
+            @Override
+            public boolean canEnchantItem(org.bukkit.inventory.ItemStack itemStack) {
+                return true;
+            }
+
+            @Override
+            public boolean isTreasure() {
+                return false;
+            }
+
+            @Override
+            public boolean isCursed() {
+                return false;
+            }
+        };
+    }
+
+    @Override
+    public boolean isOutsideWorldborder(Location location) {
+        WorldBorder worldBorder = location.getWorld().getWorldBorder();
+        int radius = (int) worldBorder.getSize() / 2;
+        return location.getBlockX() > (worldBorder.getCenter().getBlockX() + radius) || location.getBlockX() < (worldBorder.getCenter().getBlockX() - radius) ||
+                location.getBlockZ() > (worldBorder.getCenter().getBlockZ() + radius) || location.getBlockZ() < (worldBorder.getCenter().getBlockZ() - radius);
+    }
+
+    @Override
+    public Object getBlockData(Material type, byte data) {
+        return CraftBlockData.fromData(CraftMagicNumbers.getBlock(type, data));
+    }
+
+    @Override
+    public void playPickupAnimation(LivingEntity livingEntity, Item item) {
+        EntityLiving entityLiving = ((CraftLivingEntity) livingEntity).getHandle();
+        EntityItem entityItem = (EntityItem) ((CraftItem) item).getHandle();
+        ((WorldServer) entityLiving.world).getChunkProvider().broadcast(entityItem, new PacketPlayOutCollect(entityItem.getId(), entityLiving.getId(), item.getItemStack().getAmount()));
+    }
+
+    @Override
+    public org.bukkit.inventory.ItemStack[] parseChoice(Recipe recipe, org.bukkit.inventory.ItemStack itemStack) {
+        List<org.bukkit.inventory.ItemStack> ingredients = new ArrayList<>();
+        List<RecipeChoice> recipeChoices = new ArrayList<>();
+
+        ingredients.add(itemStack);
+
+        if (recipe instanceof ShapedRecipe) {
+            recipeChoices.addAll(((ShapedRecipe) recipe).getChoiceMap().values());
+        } else if (recipe instanceof ShapelessRecipe) {
+            recipeChoices.addAll(((ShapelessRecipe) recipe).getChoiceList());
+        }
+
+        if(!recipeChoices.isEmpty()) {
+            for (RecipeChoice recipeChoice : recipeChoices) {
+                if (recipeChoice instanceof RecipeChoice.MaterialChoice && recipeChoice.test(itemStack)) {
+                    ingredients.clear();
+                    for (Material material : ((RecipeChoice.MaterialChoice) recipeChoice).getChoices())
+                        ingredients.add(new org.bukkit.inventory.ItemStack(material));
+                    break;
+                }
+            }
+        }
+
+        return ingredients.toArray(new org.bukkit.inventory.ItemStack[0]);
+    }
+
+}
