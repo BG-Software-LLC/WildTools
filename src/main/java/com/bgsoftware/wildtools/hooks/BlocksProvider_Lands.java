37da21fa11b89718c187dee9649e8b8880a552ae
OmerBenGera <omerthepro126@gmail.com>
2020-02-28 12:36:58 +0200

    Updated support for Lands and removed old support

--- src/main/java/com/bgsoftware/wildtools/hooks/BlocksProvider_Lands.java
+++ src/main/java/com/bgsoftware/wildtools/hooks/BlocksProvider_Lands.java
@@ -2,10 +2,8 @@ package com.bgsoftware.wildtools.hooks;
 
 import com.bgsoftware.wildtools.WildToolsPlugin;
 
-import me.angeschossen.lands.api.enums.LandsAction;
 import me.angeschossen.lands.api.integration.LandsIntegration;
-import me.angeschossen.lands.api.landsaddons.LandsAddon;
-import me.angeschossen.lands.api.objects.LandChunk;
+import me.angeschossen.lands.api.land.LandArea;
 import me.angeschossen.lands.api.role.enums.RoleSetting;
 
 import org.bukkit.block.Block;
@@ -14,81 +12,28 @@ import org.bukkit.inventory.InventoryHolder;
 
 public final class BlocksProvider_Lands implements BlocksProvider {
 
-    private BlocksProvider landsProvider;
+    private LandsIntegration landsIntegration;
 
     public BlocksProvider_Lands(){
-        try{
-            Class.forName("me.angeschossen.lands.api.land.LandChunk");
-            landsProvider = new LandsNew();
-        }catch(Throwable ex){
-            landsProvider = new LandsOld();
-        }
+        landsIntegration = new LandsIntegration(WildToolsPlugin.getPlugin(), false);
+        landsIntegration.initialize();
     }
 
     @Override
     public boolean canBreak(Player player, Block block, boolean onlyInClaim) {
-        return landsProvider.canBreak(player, block, onlyInClaim);
+        LandArea landArea = landsIntegration.getArea(block.getLocation());
+        if(onlyInClaim && landArea == null) return false;
+        return landArea == null || landArea.canSetting(player.getUniqueId(), RoleSetting.BLOCK_PLACE);
     }
 
     @Override
     public boolean canInteract(Player player, Block block, boolean onlyInClaim) {
-        return landsProvider.canInteract(player, block, onlyInClaim);
-    }
+        LandArea landArea = landsIntegration.getArea(block.getLocation());
+        if(onlyInClaim && landArea == null) return false;
 
-    private static final class LandsOld implements BlocksProvider {
+        RoleSetting roleSetting = block.getState() instanceof InventoryHolder ? RoleSetting.INTERACT_CONTAINER : RoleSetting.INTERACT_GENERAL;
 
-        private LandsAddon landsAddon;
-
-        LandsOld(){
-            landsAddon = new LandsAddon(WildToolsPlugin.getPlugin(), false);
-            landsAddon.initialize();
-        }
-
-        @Override
-        public boolean canBreak(Player player, Block block, boolean onlyInClaim) {
-            LandChunk landChunk = landsAddon.getLandChunkHard(block.getWorld().getName(), block.getChunk().getX(), block.getChunk().getZ());
-            if(onlyInClaim && landChunk == null) return false;
-            return landChunk == null || landChunk.canAction(player.getUniqueId().toString(), LandsAction.BLOCK_PLACE);
-        }
-
-        @Override
-        public boolean canInteract(Player player, Block block, boolean onlyInClaim) {
-            LandChunk landChunk = landsAddon.getLandChunkHard(block.getWorld().getName(), block.getChunk().getX(), block.getChunk().getZ());
-            if(onlyInClaim && landChunk == null) return false;
-
-            LandsAction landsAction = block.getState() instanceof InventoryHolder ? LandsAction.INTERACT_CONTAINER : LandsAction.INTERACT_GENERAL;
-
-            return landChunk == null || landChunk.canAction(player.getUniqueId().toString(), landsAction);
-        }
-
-    }
-
-    private static final class LandsNew implements BlocksProvider {
-
-        private LandsIntegration landsIntegration;
-
-        LandsNew(){
-            landsIntegration = new LandsIntegration(WildToolsPlugin.getPlugin(), false);
-            landsIntegration.initialize();
-        }
-
-        @Override
-        public boolean canBreak(Player player, Block block, boolean onlyInClaim) {
-            me.angeschossen.lands.api.land.LandChunk landChunk = landsIntegration.getLandChunk(block.getLocation());
-            if(onlyInClaim && landChunk == null) return false;
-            return landChunk == null || landChunk.canAction(player.getUniqueId(), RoleSetting.BLOCK_PLACE);
-        }
-
-        @Override
-        public boolean canInteract(Player player, Block block, boolean onlyInClaim) {
-            me.angeschossen.lands.api.land.LandChunk landChunk = landsIntegration.getLandChunk(block.getLocation());
-            if(onlyInClaim && landChunk == null) return false;
-
-            RoleSetting roleSetting = block.getState() instanceof InventoryHolder ? RoleSetting.INTERACT_CONTAINER : RoleSetting.INTERACT_GENERAL;
-
-            return landChunk == null || landChunk.canAction(player.getUniqueId(), roleSetting);
-        }
-
+        return landArea == null || landArea.canSetting(player.getUniqueId(), roleSetting);
     }
 
 }
