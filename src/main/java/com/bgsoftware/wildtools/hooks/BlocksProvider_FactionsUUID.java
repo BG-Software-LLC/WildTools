43b6ed5e96ac5fbddb85b2052949aa803b5e2aee
OmerBenGera <omerthepro126@gmail.com>
2020-03-29 22:17:52 +0300

    Support for newer versions of factions

--- src/main/java/com/bgsoftware/wildtools/hooks/BlocksProvider_FactionsUUID.java
+++ src/main/java/com/bgsoftware/wildtools/hooks/BlocksProvider_FactionsUUID.java
@@ -9,7 +9,23 @@ import com.massivecraft.factions.struct.Relation;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
+import java.lang.reflect.Method;
+
 public final class BlocksProvider_FactionsUUID implements BlocksProvider {
+
+    private static Method getRelationWithMethod;
+    private static Object allyRelationEnum;
+
+    static {
+        try{
+            getRelationWithMethod = Faction.class.getDeclaredMethod("getRelationWish", Faction.class);
+            Class<?> relationClass = Class.forName("com.massivecraft.factions.perms.Relation");
+            for(Object en : relationClass.getEnumConstants()){
+                if("ALLY".equals(en + ""))
+                    allyRelationEnum = en;
+            }
+        }catch(Throwable ignored){}
+    }
 
     @Override
     public boolean canBreak(Player player, Block block, boolean onlyInClaim) {
@@ -17,6 +33,15 @@ public final class BlocksProvider_FactionsUUID implements BlocksProvider {
         Faction faction = Board.getInstance().getFactionAt(new FLocation(block.getLocation()));
         if(onlyInClaim && faction.isWilderness()) return false;
         return faction.isWilderness() || fPlayer.isAdminBypassing() || (fPlayer.hasFaction() && (fPlayer.getFaction().equals(faction) ||
-                faction.getRelationWish(fPlayer.getFaction()) == Relation.ALLY));
+                hasRelation(faction, fPlayer)));
     }
+
+    private boolean hasRelation(Faction faction, FPlayer fPlayer){
+        try {
+            return getRelationWithMethod.invoke(faction, fPlayer.getFaction()) == allyRelationEnum;
+        }catch(Throwable ex){
+            return faction.getRelationWish(fPlayer.getFaction()) == Relation.ALLY;
+        }
+    }
+
 }
