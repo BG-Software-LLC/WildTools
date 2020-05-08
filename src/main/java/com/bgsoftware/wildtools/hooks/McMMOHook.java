4ae6f36e3f678ec96b1bb18cce937443bdfaee04
OmerBenGera <omerthepro126@gmail.com>
2020-05-08 21:31:54 +0300

    Added support for older versions of mcmmo

--- src/main/java/com/bgsoftware/wildtools/hooks/McMMOHook.java
+++ src/main/java/com/bgsoftware/wildtools/hooks/McMMOHook.java
@@ -1,6 +1,8 @@
 package com.bgsoftware.wildtools.hooks;
 
+import com.gmail.nossr50.datatypes.player.McMMOPlayer;
 import com.gmail.nossr50.datatypes.skills.PrimarySkillType;
+import com.gmail.nossr50.datatypes.skills.SkillType;
 import com.gmail.nossr50.datatypes.skills.SuperAbilityType;
 import com.gmail.nossr50.mcMMO;
 import com.gmail.nossr50.skills.excavation.ExcavationManager;
@@ -17,9 +19,21 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.lang.reflect.Method;
+
+@SuppressWarnings("JavaReflectionMemberAccess")
 public final class McMMOHook {
 
-    private static final boolean isEnabled = isSupportedBuild();
+    private static final boolean isEnabled = Bukkit.getPluginManager().isPluginEnabled("mcMMO");
+    private static Method checkAbilityActivationMethod = null;
+    private static Method herbalismBlockCheckMethod = null;
+
+    static {
+        try{
+            checkAbilityActivationMethod = McMMOPlayer.class.getMethod("checkAbilityActivation", SkillType.class);
+            herbalismBlockCheckMethod = HerbalismManager.class.getMethod("herbalismBlockCheck", BlockState.class);
+        }catch(Throwable ignored){}
+    }
 
     public static void markAsPlaced(Location loc){
         if(isEnabled)
@@ -33,19 +47,20 @@ public final class McMMOHook {
             if (BlockUtils.affectedByGreenTerra(block.getState())) {
                 HerbalismManager herbalismManager = mcMMOPlayer.getHerbalismManager();
                 if (herbalismManager.canActivateAbility()) {
-                    mcMMOPlayer.checkAbilityActivation(PrimarySkillType.HERBALISM);
+                    checkAbilityActivation(mcMMOPlayer);
                 }
 
-                if (PrimarySkillType.HERBALISM.getPermissions(player)) {
-                    herbalismManager.processHerbalismBlockBreakEvent(new BlockBreakEvent(block, player));
+                if (getPermissions("HERBALISM", player)) {
+                    herbalismBlockCheck(herbalismManager, block, player);
                 }
 
             } else {
                 if (BlockUtils.affectedBySuperBreaker(blockState) && ItemUtils.isPickaxe(inHand) &&
-                        PrimarySkillType.MINING.getPermissions(player) && !mcMMO.getPlaceStore().isTrue(blockState)) {
+                        getPermissions("MINING", player) && !mcMMO.getPlaceStore().isTrue(blockState)) {
                     MiningManager miningManager = mcMMOPlayer.getMiningManager();
                     miningManager.miningBlockCheck(blockState);
-                } else if (BlockUtils.isLog(blockState) && ItemUtils.isAxe(inHand) && PrimarySkillType.WOODCUTTING.getPermissions(player) && !mcMMO.getPlaceStore().isTrue(blockState)) {
+                } else if (BlockUtils.isLog(blockState) && ItemUtils.isAxe(inHand) && getPermissions("WOODCUTTING", player) &&
+                        !mcMMO.getPlaceStore().isTrue(blockState)) {
                     WoodcuttingManager woodcuttingManager = mcMMOPlayer.getWoodcuttingManager();
                     if (woodcuttingManager.canUseTreeFeller(inHand)) {
                         woodcuttingManager.processTreeFeller(blockState);
@@ -53,7 +68,7 @@ public final class McMMOHook {
                         woodcuttingManager.woodcuttingBlockCheck(blockState);
                     }
                 } else if (BlockUtils.affectedByGigaDrillBreaker(blockState) && ItemUtils.isShovel(inHand) &&
-                        PrimarySkillType.EXCAVATION.getPermissions(player) && !mcMMO.getPlaceStore().isTrue(blockState)) {
+                        getPermissions("EXCAVATION", player) && !mcMMO.getPlaceStore().isTrue(blockState)) {
                     ExcavationManager excavationManager = mcMMOPlayer.getExcavationManager();
                     excavationManager.excavationBlockCheck(blockState);
                     if (mcMMOPlayer.getAbilityMode(SuperAbilityType.GIGA_DRILL_BREAKER)) {
@@ -66,12 +81,27 @@ public final class McMMOHook {
         }
     }
 
-    private static boolean isSupportedBuild(){
+    private static boolean getPermissions(String type, Player player){
         try{
-            Class.forName("com.gmail.nossr50.datatypes.skills.PrimarySkillType");
-            return true;
-        }catch(ClassNotFoundException ex){
-            return false;
+            return PrimarySkillType.valueOf(type).getPermissions(player);
+        }catch(Throwable ex){
+            return SkillType.valueOf(type).getPermissions(player);
+        }
+    }
+
+    private static void checkAbilityActivation(McMMOPlayer mcMMOPlayer){
+        try{
+            checkAbilityActivationMethod.invoke(mcMMOPlayer, SkillType.valueOf("HERBALISM"));
+        }catch(Throwable ex){
+            mcMMOPlayer.checkAbilityActivation(PrimarySkillType.valueOf("HERBALISM"));
+        }
+    }
+
+    private static void herbalismBlockCheck(HerbalismManager herbalismManager, Block block, Player player){
+        try{
+            herbalismBlockCheckMethod.invoke(herbalismManager, block.getState());
+        }catch(Throwable ex){
+            herbalismManager.processHerbalismBlockBreakEvent(new BlockBreakEvent(block, player));
         }
     }
 
