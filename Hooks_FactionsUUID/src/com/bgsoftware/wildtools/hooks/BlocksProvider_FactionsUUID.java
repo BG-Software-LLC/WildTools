bb19c13f9595df25a461d52f8adaa9a1a34b6adf
OmerBenGera <omerthepro126@gmail.com>
2020-05-30 22:59:34 +0300

    Fixed detection of build access of Factions

--- Hooks_FactionsUUID/src/com/bgsoftware/wildtools/hooks/BlocksProvider_FactionsUUID.java
+++ Hooks_FactionsUUID/src/com/bgsoftware/wildtools/hooks/BlocksProvider_FactionsUUID.java
@@ -2,30 +2,29 @@ package com.bgsoftware.wildtools.hooks;
 
 import com.massivecraft.factions.Board;
 import com.massivecraft.factions.FLocation;
-import com.massivecraft.factions.FPlayer;
-import com.massivecraft.factions.FPlayers;
 import com.massivecraft.factions.Faction;
 import com.massivecraft.factions.FactionsPlugin;
 import com.massivecraft.factions.P;
 import com.massivecraft.factions.SavageFactions;
-import com.massivecraft.factions.perms.Permissible;
+import com.massivecraft.factions.listeners.FactionsBlockListener;
 import com.massivecraft.factions.perms.PermissibleAction;
-import com.massivecraft.factions.zcore.fperms.Access;
 import com.massivecraft.factions.zcore.fperms.PermissableAction;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.lang.reflect.Method;
-import java.util.Map;
 
 public final class BlocksProvider_FactionsUUID implements BlocksProvider {
 
-    private static Method getRelationWithMethod;
+    private static Method playerCanBuildDestroyBlockMethod;
 
     static {
         try{
-            getRelationWithMethod = Faction.class.getDeclaredMethod("getRelationWish", Faction.class);
+            //noinspection JavaReflectionMemberAccess
+            playerCanBuildDestroyBlockMethod = FactionsBlockListener.class.getMethod("playerCanBuildDestroyBlock",
+                    Player.class, Location.class, PermissibleAction.class, boolean.class);
         }catch(Throwable ignored){}
     }
 
@@ -45,30 +44,23 @@ public final class BlocksProvider_FactionsUUID implements BlocksProvider {
 
     @Override
     public boolean canBreak(Player player, Block block, boolean onlyInClaim) {
-        FPlayer fPlayer = FPlayers.getInstance().getByPlayer(player);
         Faction faction = Board.getInstance().getFactionAt(new FLocation(block.getLocation()));
         if(onlyInClaim && faction.isWilderness()) return false;
-        return faction.isWilderness() || fPlayer.isAdminBypassing() || (fPlayer.hasFaction() && (fPlayer.getFaction().equals(faction) ||
-                hasRelation(faction, fPlayer, "DESTROY")));
+        return playerCanBuildDestroyBlockMethod(player, block, PermissableAction.DESTROY);
     }
 
     @Override
     public boolean canInteract(Player player, Block block, boolean onlyInClaim) {
-        FPlayer fPlayer = FPlayers.getInstance().getByPlayer(player);
         Faction faction = Board.getInstance().getFactionAt(new FLocation(block.getLocation()));
         if(onlyInClaim && faction.isWilderness()) return false;
-        return faction.isWilderness() || fPlayer.isAdminBypassing() || (fPlayer.hasFaction() && (fPlayer.getFaction().equals(faction) ||
-                hasRelation(faction, fPlayer, "CONTAINER")));
+        return playerCanBuildDestroyBlockMethod(player, block, PermissableAction.CONTAINER);
     }
 
-    @SuppressWarnings("all")
-    private boolean hasRelation(Faction faction, FPlayer fPlayer, String permission){
-        try {
-            Permissible permissible = (Permissible) getRelationWithMethod.invoke(faction, fPlayer.getFaction());
-            return (Boolean) ((Map) faction.getPermissions().get(permissible)).get(PermissibleAction.valueOf(permission));
+    private boolean playerCanBuildDestroyBlockMethod(Player player, Block block, PermissableAction action){
+        try{
+            return (boolean) playerCanBuildDestroyBlockMethod.invoke(player, block.getLocation(), action, true);
         }catch(Throwable ex){
-            return faction.getPermissions().get(faction.getRelationWish(fPlayer.getFaction()))
-                    .get(PermissableAction.valueOf(permission)) == Access.ALLOW;
+            return FactionsBlockListener.playerCanBuildDestroyBlock(player, block.getLocation(), action.getName(), true);
         }
     }
 
