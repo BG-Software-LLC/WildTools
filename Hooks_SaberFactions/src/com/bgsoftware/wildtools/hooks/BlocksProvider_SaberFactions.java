521a25d35e779a5061aacdcc5caf4621576e2217
OmerBenGera <omerthepro126@gmail.com>
2020-07-05 13:06:41 +0300

    Fixed issues with SaberFactions

--- Hooks_SaberFactions/src/com/bgsoftware/wildtools/hooks/BlocksProvider_SaberFactions.java
+++ Hooks_SaberFactions/src/com/bgsoftware/wildtools/hooks/BlocksProvider_SaberFactions.java
@@ -6,11 +6,24 @@ import com.massivecraft.factions.Faction;
 import com.massivecraft.factions.FactionsPlugin;
 import com.massivecraft.factions.listeners.FactionsBlockListener;
 import com.massivecraft.factions.zcore.fperms.PermissableAction;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import java.lang.reflect.Method;
+
 public final class BlocksProvider_SaberFactions implements BlocksProvider {
+
+    private static Method playerCanBuildDestroyBlockMethod;
+
+    static {
+        try{
+            //noinspection JavaReflectionMemberAccess
+            playerCanBuildDestroyBlockMethod = FactionsBlockListener.class.getMethod("playerCanBuildDestroyBlock",
+                    Player.class, Location.class, PermissableAction.class, boolean.class);
+        }catch(Throwable ignored){}
+    }
 
     @Override
     public Plugin getPlugin() {
@@ -21,14 +34,22 @@ public final class BlocksProvider_SaberFactions implements BlocksProvider {
     public boolean canBreak(Player player, Block block, boolean onlyInClaim) {
         Faction faction = Board.getInstance().getFactionAt(new FLocation(block.getLocation()));
         if(onlyInClaim && faction.isWilderness()) return false;
-        return FactionsBlockListener.playerCanBuildDestroyBlock(player, block.getLocation(), PermissableAction.DESTROY, true);
+        return playerCanBuildDestroyBlockMethod(player, block, "DESTROY");
     }
 
     @Override
     public boolean canInteract(Player player, Block block, boolean onlyInClaim) {
         Faction faction = Board.getInstance().getFactionAt(new FLocation(block.getLocation()));
         if(onlyInClaim && faction.isWilderness()) return false;
-        return FactionsBlockListener.playerCanBuildDestroyBlock(player, block.getLocation(), PermissableAction.CONTAINER, true);
+        return playerCanBuildDestroyBlockMethod(player, block, "CONTAINER");
+    }
+
+    private boolean playerCanBuildDestroyBlockMethod(Player player, Block block, String action){
+        try{
+            return (boolean) playerCanBuildDestroyBlockMethod.invoke(null, player, block.getLocation(), PermissableAction.valueOf(action), true);
+        }catch(Throwable ex){
+            return FactionsBlockListener.playerCanBuildDestroyBlock(player, block.getLocation(), action, true);
+        }
     }
 
 }
