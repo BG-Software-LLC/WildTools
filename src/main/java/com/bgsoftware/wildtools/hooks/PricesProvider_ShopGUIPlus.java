8599b00af17718aa950b5c0e467ad7f9e30268e6
OmerBenGera <omerthepro126@gmail.com>
2020-09-21 14:19:10 +0300

    Optimized the hook of ShopGUIPlus

--- src/main/java/com/bgsoftware/wildtools/hooks/PricesProvider_ShopGUIPlus.java
+++ src/main/java/com/bgsoftware/wildtools/hooks/PricesProvider_ShopGUIPlus.java
@@ -2,15 +2,23 @@ package com.bgsoftware.wildtools.hooks;
 
 import com.bgsoftware.wildtools.WildToolsPlugin;
 import com.bgsoftware.wildtools.api.hooks.PricesProvider;
+import com.bgsoftware.wildtools.utils.Pair;
 import net.brcdev.shopgui.ShopGuiPlugin;
-import net.brcdev.shopgui.ShopGuiPlusApi;
 
+import net.brcdev.shopgui.player.PlayerData;
+import net.brcdev.shopgui.shop.Shop;
+import net.brcdev.shopgui.shop.ShopItem;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public final class PricesProvider_ShopGUIPlus implements PricesProvider {
 
-    private ShopGuiPlugin plugin;
+    // Added cache for shop items for better performance
+    private final Map<WrappedItemStack, Pair<ShopItem, Shop>> cachedShopItems = new HashMap<>();
+    private final ShopGuiPlugin plugin;
 
     public PricesProvider_ShopGUIPlus(){
         WildToolsPlugin.log(" - Using ShopGUIPlus as PricesProvider.");
@@ -19,11 +27,59 @@ public final class PricesProvider_ShopGUIPlus implements PricesProvider {
 
     @Override
     public double getPrice(Player player, ItemStack itemStack) {
-        try {
-            return ShopGuiPlusApi.getItemStackPriceSell(player, itemStack);
-        }catch(Exception ex){
-            plugin.getPlayerManager().registerPlayer(player);
-            return ShopGuiPlusApi.getItemStackPriceSell(player, itemStack);
+        double price = 0;
+
+        WrappedItemStack wrappedItemStack = new WrappedItemStack(itemStack);
+        Pair<ShopItem, Shop> shopPair = cachedShopItems.computeIfAbsent(wrappedItemStack, i -> {
+            Map<String, Shop> shops = plugin.getShopManager().shops;
+            for (Shop shop : shops.values()) {
+                for (ShopItem _shopItem : shop.getShopItems())
+                    if(areSimilar(_shopItem.getItem(), itemStack, _shopItem.isCompareMeta()))
+                        return new Pair<>(_shopItem, shop);
+            }
+
+            return null;
+        });
+
+        if(shopPair != null){
+            if(player == null) {
+                //noinspection deprecation
+                price = Math.max(price, shopPair.getX().getSellPriceForAmount(itemStack.getAmount()));
+            }
+            else{
+                PlayerData playerData = ShopGuiPlugin.getInstance().getPlayerManager().getPlayerData(player);
+                price = Math.max(price, shopPair.getX().getSellPriceForAmount(shopPair.getY(), player, playerData, itemStack.getAmount()));
+            }
+        }
+
+        return price;
+    }
+
+    private static boolean areSimilar(ItemStack is1, ItemStack is2, boolean compareMetadata){
+        return compareMetadata ? is1.isSimilar(is2) : is2 != null && is1 != null && is1.getType() == is2.getType() &&
+                is1.getDurability() == is2.getDurability();
+    }
+
+    private static final class WrappedItemStack{
+
+        private final ItemStack value;
+
+        WrappedItemStack(ItemStack value){
+            this.value = value.clone();
+            this.value.setAmount(1);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            WrappedItemStack that = (WrappedItemStack) o;
+            return value.equals(that.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return value.hashCode();
         }
     }
 
