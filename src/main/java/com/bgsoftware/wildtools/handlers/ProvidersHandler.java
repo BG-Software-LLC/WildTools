b55da711069846b3ac8224ad753e8cce752b1d88
OmerBenGera <omerthepro126@gmail.com>
2021-12-25 22:56:01 +0200

    Moved Villages to its own module

--- src/main/java/com/bgsoftware/wildtools/handlers/ProvidersHandler.java
+++ src/main/java/com/bgsoftware/wildtools/handlers/ProvidersHandler.java
@@ -7,7 +7,6 @@ import com.bgsoftware.wildtools.api.hooks.ContainerProvider;
 import com.bgsoftware.wildtools.api.hooks.DropsProvider;
 import com.bgsoftware.wildtools.api.hooks.PricesProvider;
 import com.bgsoftware.wildtools.api.hooks.SellInfo;
-import com.bgsoftware.wildtools.hooks.ClaimsProvider_Villages;
 import com.bgsoftware.wildtools.hooks.ContainerProvider_Default;
 import com.bgsoftware.wildtools.hooks.ContainerProvider_WildChests;
 import com.bgsoftware.wildtools.hooks.DropsProvider_RoseStacker;
@@ -348,7 +347,8 @@ public final class ProvidersHandler implements ProvidersManager {
             claimsProvider.ifPresent(this::addClaimsProvider);
         }
         if (Bukkit.getPluginManager().isPluginEnabled("Villages")) {
-            addClaimsProvider(new ClaimsProvider_Villages());
+            Optional<ClaimsProvider> claimsProvider = createInstance("ClaimsProvider_Villages");
+            claimsProvider.ifPresent(this::addClaimsProvider);
         }
     }
 
