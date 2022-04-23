e59b700e664f685e74386b2389be6a7c667ff2c3
OmerBenGera <omerthepro126@gmail.com>
2022-04-23 10:29:42 +0300

    Fixed incorrect usage of getBlockDrops command

--- src/main/java/com/bgsoftware/wildtools/objects/tools/WCrowbarTool.java
+++ src/main/java/com/bgsoftware/wildtools/objects/tools/WCrowbarTool.java
@@ -12,13 +12,14 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class WCrowbarTool extends WTool implements CrowbarTool {
 
     private final List<String> commandsOnUse;
 
-    public WCrowbarTool(Material type, String name, List<String> commandsOnUse){
+    public WCrowbarTool(Material type, String name, List<String> commandsOnUse) {
         super(type, name, ToolMode.CROWBAR);
         this.commandsOnUse = commandsOnUse;
     }
@@ -30,21 +31,23 @@ public final class WCrowbarTool extends WTool implements CrowbarTool {
 
     @Override
     public boolean onBlockInteract(PlayerInteractEvent e) {
-        if(!e.getClickedBlock().getType().name().contains("SPAWNER"))
+        if (!e.getClickedBlock().getType().name().contains("SPAWNER"))
             return false;
 
-        if(!BukkitUtils.canBreakBlock(e.getPlayer(), e.getClickedBlock(), this))
+        if (!BukkitUtils.canBreakBlock(e.getPlayer(), e.getClickedBlock(), this))
             return true;
 
         CreatureSpawner creatureSpawner = (CreatureSpawner) e.getClickedBlock().getState();
 
-        List<ItemStack> itemsToDrop = plugin.getProviders().getBlockDrops(e.getPlayer(), e.getClickedBlock(), true);
+        List<ItemStack> itemsToDrop = new ArrayList<>();
+
+        plugin.getProviders().getBlockDrops(itemsToDrop, e.getPlayer(), e.getClickedBlock(), true);
 
         boolean addedSilktouch = false;
 
         /* Many plugins require the players to have silk touch.
            Therefore, I add silk touch manually to avoid issues. */
-        if(e.getItem().getEnchantmentLevel(Enchantment.SILK_TOUCH) < 1){
+        if (e.getItem().getEnchantmentLevel(Enchantment.SILK_TOUCH) < 1) {
             addedSilktouch = true;
             e.getItem().addUnsafeEnchantment(Enchantment.SILK_TOUCH, 1);
         }
@@ -52,21 +55,20 @@ public final class WCrowbarTool extends WTool implements CrowbarTool {
         try {
             if (!BukkitUtils.breakBlock(e.getPlayer(), null, null, e.getClickedBlock(), e.getItem(), this, itemStack -> null))
                 return true;
-        }finally {
-            if(addedSilktouch)
+        } finally {
+            if (addedSilktouch)
                 e.getItem().removeEnchantment(Enchantment.SILK_TOUCH);
         }
 
-        if(commandsOnUse.isEmpty()) {
-            if(!itemsToDrop.isEmpty()) {
+        if (commandsOnUse.isEmpty()) {
+            if (!itemsToDrop.isEmpty()) {
                 ItemStack dropItem = itemsToDrop.get(0);
                 if (isAutoCollect())
                     ItemUtils.addItem(dropItem, e.getPlayer().getInventory(), e.getClickedBlock().getLocation(), null);
                 else
                     e.getClickedBlock().getWorld().dropItemNaturally(e.getClickedBlock().getLocation(), dropItem);
             }
-        }
-        else{
+        } else {
             commandsOnUse.forEach(commandOnUse -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), commandOnUse
                     .replace("%player%", e.getPlayer().getName())
                     .replace("%entity%", creatureSpawner.getSpawnedType().name())
