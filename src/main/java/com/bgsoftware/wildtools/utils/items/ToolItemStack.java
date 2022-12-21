ff7ea2f6fdc204d259eb98232a3daf6e57c9628a
OmerBenGera <omerthepro126@gmail.com>
2022-12-21 10:28:00 +0200

    Fixed a NPE when hitting stuff with empty hands (#135)

--- src/main/java/com/bgsoftware/wildtools/utils/items/ToolItemStack.java
+++ src/main/java/com/bgsoftware/wildtools/utils/items/ToolItemStack.java
@@ -8,23 +8,23 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 public abstract class ToolItemStack {
 
     private static final WildToolsPlugin plugin = WildToolsPlugin.getPlugin();
 
     public static ToolItemStack of(Material type) {
-        return of(type, 1);
+        return type == Material.AIR ? EmptyToolItemStack.INSTANCE : createFromItemStack(new ItemStack(type, 1));
     }
 
-    public static ToolItemStack of(Material type, int amount) {
-        return of(type, amount, (short) 0);
+    public static ToolItemStack of(@Nullable ItemStack itemStack) {
+        return itemStack == null || itemStack.getType() == Material.AIR ? EmptyToolItemStack.INSTANCE :
+                createFromItemStack(itemStack);
     }
 
-    public static ToolItemStack of(Material type, int amount, short damage) {
-        return of(new ItemStack(type, amount, damage));
-    }
-
-    public static ToolItemStack of(ItemStack itemStack) {
+    private static ToolItemStack createFromItemStack(@Nonnull ItemStack itemStack) {
         return plugin.getNMSAdapter().createToolItemStack(itemStack);
     }
 
@@ -43,6 +43,7 @@ public abstract class ToolItemStack {
         return handle;
     }
 
+    @Nullable
     public Tool getTool() {
         return tool;
     }
@@ -140,5 +141,154 @@ public abstract class ToolItemStack {
     public abstract String getTag(String key, String def);
 
     public abstract void breakTool(Player player);
+
+    private static class EmptyToolItemStack extends ToolItemStack {
+
+        private static final EmptyToolItemStack INSTANCE = new EmptyToolItemStack();
+
+        private final ItemStack EMPTY_ITEM = new ItemStack(Material.AIR);
+
+        @Override
+        protected void setItem(ItemStack handle) {
+            // Do nothing.
+        }
+
+        @Override
+        public ItemStack getItem() {
+            return EMPTY_ITEM;
+        }
+
+        @Nullable
+        @Override
+        public Tool getTool() {
+            return null;
+        }
+
+        @Override
+        public boolean hasSellMode() {
+            return false;
+        }
+
+        @Override
+        public void setSellMode(boolean sellMode) {
+            // Do nothing.
+        }
+
+        @Override
+        public void setUses(int uses) {
+            // Do nothing.
+        }
+
+        @Override
+        public int getUses() {
+            return 0;
+        }
+
+        @Override
+        public String getToolType() {
+            return "";
+        }
+
+        @Override
+        public void setToolType(String toolType) {
+            // Do nothing.
+        }
+
+        @Override
+        public String getOwner() {
+            return "";
+        }
+
+        @Override
+        public void setOwner(String owner) {
+            // Do nothing.
+        }
+
+        @Override
+        public ItemMeta getItemMeta() {
+            return null;
+        }
+
+        @Override
+        public void setItemMeta(ItemMeta itemMeta) {
+            // Do nothing.
+        }
+
+        @Override
+        public boolean hasItemMeta() {
+            return false;
+        }
+
+        @Override
+        public Material getType() {
+            return Material.AIR;
+        }
+
+        @Override
+        public void setType(Material type) {
+            // Do nothing.
+        }
+
+        @Override
+        public int getAmount() {
+            return 0;
+        }
+
+        @Override
+        public void setAmount(int amount) {
+            // Do nothing.
+        }
+
+        @Override
+        public int getEnchantmentLevel(Enchantment enchantment) {
+            return 0;
+        }
+
+        @Override
+        public void setDurability(short durability) {
+            // Do nothing.
+        }
+
+        @Override
+        public short getDurability() {
+            return 0;
+        }
+
+        @Override
+        public short getMaxDurability() {
+            return 0;
+        }
+
+        @Override
+        public ToolItemStack copy() {
+            return this;
+        }
+
+        @Override
+        public void setTag(String key, int value) {
+            // Do nothing
+        }
+
+        @Override
+        public void setTag(String key, String value) {
+            // Do nothing
+        }
+
+        @Override
+        public int getTag(String key, int def) {
+            return def;
+        }
+
+        @Override
+        public String getTag(String key, String def) {
+            return def;
+        }
+
+        @Override
+        public void breakTool(Player player) {
+            // Do nothing
+        }
+
+    }
 
 }
