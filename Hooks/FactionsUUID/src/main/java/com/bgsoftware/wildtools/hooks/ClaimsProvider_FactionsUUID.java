407a27cd0067d47779a2106db705ed130199e8a1
OmerBenGera <omerthepro126@gmail.com>
2024-08-09 18:52:25 +0300

    Fix when using SaberFactions errors are thrown due to missing class (#201)

--- Hooks/FactionsUUID/src/main/java/com/bgsoftware/wildtools/hooks/ClaimsProvider_FactionsUUID.java
+++ Hooks/FactionsUUID/src/main/java/com/bgsoftware/wildtools/hooks/ClaimsProvider_FactionsUUID.java
@@ -14,29 +14,54 @@ import org.bukkit.entity.Player;
 
 public class ClaimsProvider_FactionsUUID implements ClaimsProvider {
 
-    private static final ReflectMethod<Boolean> PLAYER_CAN_BUILD_DESTROY_BLOCK_OLD = new ReflectMethod<>(
-            FactionsBlockListener.class, "playerCanBuildDestroyBlock",
-            Player.class, Location.class, String.class, boolean.class);
-
-    private static final ReflectField<PermissibleAction> PERMISSIBLE_ACTIONS_DESTROY = new ReflectField<>(
-            new ClassInfo("com.massivecraft.factions.perms.PermissibleActions", ClassInfo.PackageType.UNKNOWN),
-            PermissibleAction.class, "DESTROY");
+    private static final IFaction FACTION_HOOK = IFaction.create();
 
     @Override
     public boolean isPlayerClaim(Player player, Location location) {
         Faction faction = Board.getInstance().getFactionAt(new FLocation(location));
-        return !faction.isWilderness() && playerCanBuildDestroyBlock(player, location);
+        return !faction.isWilderness() && FACTION_HOOK.playerCanBuildDestroyBlock(player, location);
     }
 
-    private static boolean playerCanBuildDestroyBlock(Player player, Location location) {
-        if (PLAYER_CAN_BUILD_DESTROY_BLOCK_OLD.isValid()) {
-            return PLAYER_CAN_BUILD_DESTROY_BLOCK_OLD.invoke(null, player, location, "DESTROY", true);
+    interface IFaction {
+
+        boolean playerCanBuildDestroyBlock(Player player, Location location);
+
+        static IFaction create() {
+            if (OldFactionsAPI.PLAYER_CAN_BUILD_DESTROY_BLOCK.isValid())
+                return new OldFactionsAPI();
+
+            return new NewFactionsAPI();
         }
 
-        PermissibleAction permissibleAction = PERMISSIBLE_ACTIONS_DESTROY.isValid() ?
-                PERMISSIBLE_ACTIONS_DESTROY.get(null) : PermissibleAction.DESTROY;
+    }
 
-        return FactionsBlockListener.playerCanBuildDestroyBlock(player, location, permissibleAction, true);
+    private static class OldFactionsAPI implements IFaction {
+
+        private static final ReflectMethod<Boolean> PLAYER_CAN_BUILD_DESTROY_BLOCK = new ReflectMethod<>(
+                FactionsBlockListener.class, "playerCanBuildDestroyBlock",
+                Player.class, Location.class, String.class, boolean.class);
+
+        @Override
+        public boolean playerCanBuildDestroyBlock(Player player, Location location) {
+            return PLAYER_CAN_BUILD_DESTROY_BLOCK.invoke(null, player, location, "DESTROY", true);
+        }
+
+    }
+
+    private static class NewFactionsAPI implements IFaction {
+
+        private static final ReflectField<PermissibleAction> PERMISSIBLE_ACTIONS_DESTROY = new ReflectField<>(
+                new ClassInfo("com.massivecraft.factions.perms.PermissibleActions", ClassInfo.PackageType.UNKNOWN),
+                PermissibleAction.class, "DESTROY");
+
+        @Override
+        public boolean playerCanBuildDestroyBlock(Player player, Location location) {
+            PermissibleAction permissibleAction = PERMISSIBLE_ACTIONS_DESTROY.isValid() ?
+                    PERMISSIBLE_ACTIONS_DESTROY.get(null) : PermissibleAction.DESTROY;
+
+            return FactionsBlockListener.playerCanBuildDestroyBlock(player, location, permissibleAction, true);
+        }
+
     }
 
 }
