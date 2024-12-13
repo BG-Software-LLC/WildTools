e8359fde42b095f22a9b7b6ca670abf9595bc278
OmerBenGera <omerthepro126@gmail.com>
2024-12-13 16:21:01 +0200

    Reuse BlockMaterial whenever possible

--- src/main/java/com/bgsoftware/wildtools/world/BlockMaterial.java
+++ src/main/java/com/bgsoftware/wildtools/world/BlockMaterial.java
@@ -5,9 +5,14 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class BlockMaterial {
+
+    private static final Map<Material, BlockMaterial> BLOCK_MATERIALS = initializeBlockMaterials();
 
     private final Material type;
     private final short data;
@@ -15,16 +20,24 @@ public class BlockMaterial {
     public static BlockMaterial of(Block block) {
         Material type = block.getType();
         short data = ServerVersion.isLegacy() ? block.getState().getData().toItemStack().getDurability() : 0;
-        return new BlockMaterial(type, data);
+        return of(type, data);
     }
 
     public static BlockMaterial of(BlockState blockState) {
         Material type = blockState.getType();
         short data = ServerVersion.isLegacy() ? blockState.getData().toItemStack().getDurability() : 0;
-        return new BlockMaterial(type, data);
+        return of(type, data);
     }
 
-    public BlockMaterial(Material type, short data) {
+    public static BlockMaterial of(Material type, short data) {
+        BlockMaterial blockMaterial = null;
+        if (data == 0)
+            blockMaterial = BLOCK_MATERIALS.get(type);
+
+        return blockMaterial == null ? new BlockMaterial(type, data) : blockMaterial;
+    }
+
+    private BlockMaterial(Material type, short data) {
         this.type = type;
         this.data = data;
     }
@@ -48,6 +61,17 @@ public class BlockMaterial {
     @Override
     public int hashCode() {
         return Objects.hash(type, data);
+    }
+
+    private static Map<Material, BlockMaterial> initializeBlockMaterials() {
+        EnumMap<Material, BlockMaterial> blockMaterialsMap = new EnumMap<>(Material.class);
+
+        for (Material material : Material.values()) {
+            if (material.isBlock())
+                blockMaterialsMap.put(material, new BlockMaterial(material, (short) 0));
+        }
+
+        return Collections.unmodifiableMap(blockMaterialsMap);
     }
 
 }
