ae535c7ee7907971e4d1e62ad88e7a1c835b4d4c
OmerBenGera <omerthepro126@gmail.com>
2025-10-10 12:17:14 +0300

    Adds support to 1.21.10

--- NMS/v1_21_9/src/main/java/com/bgsoftware/wildtools/nms/v1_21_9/tool/ToolItemStackImpl.java
+++ NMS/v1_21_9/src/main/java/com/bgsoftware/wildtools/nms/v1_21_9/tool/ToolItemStackImpl.java
@@ -1,14 +1,19 @@
 package com.bgsoftware.wildtools.nms.v1_21_9.tool;
 
+import com.bgsoftware.common.reflection.ReflectField;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.CustomData;
 
+import java.lang.reflect.Modifier;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class ToolItemStackImpl extends com.bgsoftware.wildtools.nms.v1_21_9.tool.AbstractToolItemStack {
+
+    private static final ReflectField<CompoundTag> CUSTOM_DATA_TAG = new ReflectField<>(CustomData.class,
+            CompoundTag.class, Modifier.PRIVATE | Modifier.FINAL, 1);
 
     public ToolItemStackImpl(ItemStack nmsItem) {
         super(nmsItem);
@@ -43,10 +48,18 @@ public class ToolItemStackImpl extends com.bgsoftware.wildtools.nms.v1_21_9.tool
     private <R> R getTagInternal(Function<CompoundTag, R> function, R def) {
         CustomData customData = this.nmsItem.get(DataComponents.CUSTOM_DATA);
         if(customData != null) {
-            CompoundTag compoundTag = customData.getUnsafe();
+            CompoundTag compoundTag = getCustomDataTag(customData);
             return function.apply(compoundTag);
         }
         return def;
+    }
+
+    private static CompoundTag getCustomDataTag(CustomData customData) {
+        try {
+            return customData.getUnsafe();
+        } catch (Throwable error) {
+            return CUSTOM_DATA_TAG.get(customData);
+        }
     }
 
 }
