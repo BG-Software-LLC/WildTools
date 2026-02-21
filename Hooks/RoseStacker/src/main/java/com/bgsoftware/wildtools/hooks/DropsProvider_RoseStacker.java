2a935a9fe66cd9d197a0110631ee286fd07faa3a
OmerBenGera <omerthepro126@gmail.com>
2026-02-21 14:29:16 +0200

    Fixed detection of crowbar breaking blocks by other plugins (#254)

--- Hooks/RoseStacker/src/main/java/com/bgsoftware/wildtools/hooks/DropsProvider_RoseStacker.java
+++ Hooks/RoseStacker/src/main/java/com/bgsoftware/wildtools/hooks/DropsProvider_RoseStacker.java
@@ -2,16 +2,16 @@ package com.bgsoftware.wildtools.hooks;
 
 import com.bgsoftware.wildtools.api.hooks.DropsProvider;
 import dev.rosewood.rosestacker.api.RoseStackerAPI;
+import dev.rosewood.rosestacker.event.SpawnerUnstackEvent;
 import dev.rosewood.rosestacker.stack.StackedSpawner;
 import dev.rosewood.rosestacker.utils.ItemUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 public class DropsProvider_RoseStacker implements DropsProvider {
@@ -25,10 +25,17 @@ public class DropsProvider_RoseStacker implements DropsProvider {
         StackedSpawner stackedSpawner = RoseStackerAPI.getInstance().getStackedSpawner(block);
 
         if (stackedSpawner != null) {
-            dropItem = ItemUtils.getSpawnerAsStackedItemStack(stackedSpawner.getSpawner().getSpawnedType(), stackedSpawner.getStackSize());
-            stackedSpawner.setStackSize(0);
-            stackedSpawner.updateDisplay();
-            RoseStackerAPI.getInstance().removeSpawnerStack(stackedSpawner);
+            int stackSize = stackedSpawner.getStackSize();
+            SpawnerUnstackEvent spawnerUnstackEvent = new SpawnerUnstackEvent(player, stackedSpawner, stackSize);
+            Bukkit.getPluginManager().callEvent(spawnerUnstackEvent);
+            int decreaseCount = spawnerUnstackEvent.getDecreaseAmount();
+
+            dropItem = ItemUtils.getSpawnerAsStackedItemStack(stackedSpawner.getSpawner().getSpawnedType(), decreaseCount);
+            stackedSpawner.increaseStackSize(-decreaseCount);
+
+            if (stackedSpawner.getStackSize() <= 0) {
+                RoseStackerAPI.getInstance().removeSpawnerStack(stackedSpawner);
+            }
         } else {
             dropItem = DropsProviders_Default.getSpawnerItem((CreatureSpawner) block.getState());
         }
