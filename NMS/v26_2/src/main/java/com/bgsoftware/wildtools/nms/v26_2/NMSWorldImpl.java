e9dc6f28ca4217a1377c6d8f9a58ae0118818977
OmerBenGera <omerthepro126@gmail.com>
2026-06-13 13:31:59 +0300

    Adds support to 26.2

---
+++ NMS/v26_2/src/main/java/com/bgsoftware/wildtools/nms/v26_2/NMSWorldImpl.java
@@ -0,0 +1,35 @@
+package com.bgsoftware.wildtools.nms.v26_2;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.protocol.game.ClientboundLightUpdatePacket;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ThreadedLevelLightEngine;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.LevelChunk;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.block.CraftBlock;
+
+public class NMSWorldImpl extends com.bgsoftware.wildtools.nms.v26_2.AbstractNMSWorld {
+
+    @Override
+    protected BlockState getBlockState(Block block) {
+        return ((CraftBlock) block).getBlockState();
+    }
+
+    @Override
+    protected int getExpDrop(BlockState blockState, ServerPlayer serverPlayer, BlockPos blockPos) {
+        return blockState.getBlock().getExpDrop(blockState, serverPlayer.level(), blockPos, serverPlayer.getMainHandItem(), true);
+    }
+
+    @Override
+    protected void setBlockState(LevelChunk levelChunk, BlockPos blockPos, BlockState blockState) {
+        levelChunk.setBlockState(blockPos, blockState, 3);
+    }
+
+    @Override
+    protected ClientboundLightUpdatePacket createLightUpdatePacket(ChunkPos chunkPos, ThreadedLevelLightEngine lightEngine) {
+        return new ClientboundLightUpdatePacket(chunkPos, lightEngine, null, null);
+    }
+
+}
