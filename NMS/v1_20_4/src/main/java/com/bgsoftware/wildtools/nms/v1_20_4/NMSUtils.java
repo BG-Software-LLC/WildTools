7ad669207e42eb72bc49ec42c83e3e5de2a6a848
Lemvs <aleksy.gajewski@wp.pl>
2026-09-24 18:18:59 +0200

    Adds support to 26.3

---
+++ NMS/v1_20_4/src/main/java/com/bgsoftware/wildtools/nms/v1_20_4/NMSUtils.java
@@ -0,0 +1,15 @@
+package com.bgsoftware.wildtools.nms.v1_20_4;
+
+import net.minecraft.world.item.ItemStack;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
+
+public class NMSUtils {
+
+    private NMSUtils() {
+    }
+
+    public static org.bukkit.inventory.ItemStack asMirror(ItemStack itemStack) {
+        return CraftItemStack.asCraftMirror(itemStack);
+    }
+
+}
---
+++ NMS/v1_21/src/main/java/com/bgsoftware/wildtools/nms/v1_21/NMSUtils.java
@@ -0,0 +1,15 @@
+package com.bgsoftware.wildtools.nms.v1_21;
+
+import net.minecraft.world.item.ItemStack;
+import org.bukkit.craftbukkit.inventory.CraftItemStack;
+
+public class NMSUtils {
+
+    private NMSUtils() {
+    }
+
+    public static org.bukkit.inventory.ItemStack asMirror(ItemStack itemStack) {
+        return CraftItemStack.asCraftMirror(itemStack);
+    }
+
+}
